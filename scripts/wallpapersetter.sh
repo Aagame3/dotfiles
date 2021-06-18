61ba5664d0949035b51362e7979cf44cef5f88dd
Aagame3 <aapeli@rautiainen.info>
2021-06-18 10:05:34 +0300

    Modified the wallpaper script to work as a cronjob

--- scripts/wallpapersetter.sh
+++ scripts/wallpapersetter.sh
@@ -4,5 +4,5 @@ DIR=$(ssh pi@apli-rb 'ls ~/wallpapers | shuf -n 1')
 COMMAND="ls ~/wallpapers/$DIR/ | shuf -n 1"
 FILE=$(ssh pi@apli-rb $COMMAND)
 
-scp pi@apli-rb:~/wallpapers/$DIR/$FILE /tmp/$FILE
-feh --bg-scale /tmp/$FILE
+scp -i /home/aapeli/.ssh/id_rsa pi@apli-rb:~/wallpapers/$DIR/$FILE /tmp/$FILE
+DISPLAY=:0 feh --bg-scale /tmp/$FILE
