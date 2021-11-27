8fda3bd0003d29f3ea2718281f3913042b4e3c1e
Aagame3 <aapeli@rautiainen.info>
2021-11-27 16:31:44 +0200

    Wallpapersetter now uses a static wallpaper

--- scripts/wallpapersetter.sh
+++ scripts/wallpapersetter.sh
@@ -1,9 +1,9 @@
 #Script for pulling a random wallpaper from the pi
 
-DIR=$(ssh pi@apli-rb 'ls ~/wallpapers | shuf -n 1')
-COMMAND="ls ~/wallpapers/$DIR/ | shuf -n 1"
-FILE=$(ssh pi@apli-rb $COMMAND)
+#DIR=$(ssh pi@apli-rb 'ls ~/wallpapers | shuf -n 1')
+#COMMAND="ls ~/wallpapers/$DIR/ | shuf -n 1"
+#FILE=$(ssh pi@apli-rb $COMMAND)
 
-scp -i /home/aapeli/.ssh/id_rsa pi@apli-rb:~/wallpapers/$DIR/$FILE /tmp/$FILE
-DISPLAY=:0 feh --bg-scale /tmp/$FILE
-betterlockscreen -u /tmp/$FILE
+#scp -i /home/aapeli/.ssh/id_rsa pi@apli-rb:~/wallpapers/$DIR/$FILE /tmp/$FILE
+DISPLAY=:0 feh --bg-scale /home/aapeli/Pictures/walls/deadgod.jpg
+betterlockscreen -u /home/aapeli/Downloads/kanyewp/wp.png
